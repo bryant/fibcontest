c2ff94663a032cf5e1ec1dc5a149eb5019b1ed13
bryant <bryant@nfkb>
2013-10-28 01:56:27 +0100

    python side. O(n) and Binet implementations.

---
+++ bigfib.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+def fib(nth):
+    i, j = 0, 1
+    for _ in xrange(nth):
+        i, j = j, i+j
+    return i
+
+def fib2(nth):
+    bigphi = (1+sqrt(5)) / 2
+    miniphi = (1-sqrt(5)) / 2
+    return int((bigphi**nth - miniphi**nth) / sqrt(5))
+
+def fib3(nth):
+    from decimal import Decimal as D
+    sqrt5 = D(5).sqrt()
+    bigphi = (1 + sqrt5) / 2
+    miniphi = (1 - sqrt5) / 2
+    return int((bigphi**nth - miniphi**nth) / sqrt5)
+
+if __name__ == "__main__":
+    with open('py.fib', 'w') as f:
+        f.write(str(fib(4374897)))
