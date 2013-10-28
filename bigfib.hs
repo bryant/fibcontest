cfba2c8ebf1c2edd726a5534291ae11bdffe3e2e
bryant <bryant@nfkb>
2013-10-28 02:05:47 +0100

    haskell impl using Daniel-Diaz/matrix .

---
+++ bigfib.hs
@@ -0,0 +1,11 @@
+-- fib = 0 : 1 : zipWith (+) fib (tail fib)
+
+-- fib n = round $ phi ** fromIntegral n / sq5
+--     where
+--     sq5 = sqrt 5 :: Double
+--     phi = (1 + sq5) / 2
+
+import Data.Matrix (fromLists, getElem)
+bigfib 0 = 0
+bigfib n = getElem 1 2 $ (fromLists [[1, 1], [1, 0]]) ^ n
+main = print $ bigfib 4374987
